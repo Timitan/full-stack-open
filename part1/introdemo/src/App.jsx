c39812e3b11e65663fce8eb88140d1c889c853e7
Timothy Tan <60724344+Timitan@users.noreply.github.com>
2025-09-25 22:00:40 -0700

    add exercise 1.1 files

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-const App = () => {
+const App1 = () => {
   const now = new Date()
   const a = 10
   const b = 20
@@ -12,6 +12,30 @@ const App = () => {
       <p>
         {a} plus {b} is {a + b}
       </p>
+    </div>
+  )
+}
+
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
