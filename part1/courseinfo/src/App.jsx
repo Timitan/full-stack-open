39c0d16fb25acfb383db9d38dfedfca2dff50d33
Timothy Tan <60724344+Timitan@users.noreply.github.com>
2025-09-27 21:07:55 -0700

    add changes for exercises 1.3-1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,37 +1,58 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   // const-definitions
   const Header = ({course}) => {
-    return <h1>{course}</h1>
+    return (
+      <h1>{course.name}</h1>
+    )
   }
 
-  const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+  const Content = ({parts}) => {
     return (
       <>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
+        {
+          parts.map(part => <Part part={part} />)
+        }
       </>
     )
   }
 
-  const Total = ({exercises1, exercises2, exercises3}) => {
+  const Part = ({part}) => 
+  {
+    return (
+      <p> {part.name} {part.exercises}</p>
+    )
+  }
+
+  const Total = ({parts}) => {
     return (
       <p>
-        Number of exercises {exercises1 + exercises2 + exercises3}
+        Number of exercises {parts.reduce((acc, part) => acc + part.exercises, 0)}
       </p>
     )
   }
@@ -39,9 +60,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1}  part2={part2} part3={part3} 
-        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
