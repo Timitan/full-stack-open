b15a37764f533628fdfa1b76194959b3d0ecb087
Timothy Tan <60724344+Timitan@users.noreply.github.com>
2025-10-13 03:13:37 -0700

    add exercises 2.6 and 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,46 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault();
+
+    // Check for duplicate names by looping through array to see if it finds a similar name
+    if(persons.some(person => {
+      return person.name === newName;
+    })) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    const nameObj = {
+      name: newName
+    };
+    setPersons(persons.concat(nameObj));
+    setNewName('');
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={addName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {
+        persons.map(person => {
+          return <p key={person.name}>{person.name}</p>
+        })
+      }
     </div>
   )
 }
