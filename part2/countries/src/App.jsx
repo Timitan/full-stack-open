51fe295cc1987480fae36ee383182e60e82ee372
Timothy Tan <60724344+Timitan@users.noreply.github.com>
2025-10-30 22:45:30 -0700

    add country search for exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import countryNames from './countryNames.js'
 
 function App() {
+  const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
+  // useEffect(() => {
+  //   axios.get('https://studies.cs.helsinki.fi/restcountries/api/all').then(response => {
+  //     console.log(response.data);
+  //     const names = response.data.map(country => country.name.common);
+  //     console.log(names);
+  //     //setCountries(response.data);
+  //   });
+  // }, []);
+
+  const handleFilterChange = (event) => {
+    const value = event.target.value;
+    setFilter(value)
+    const filteredCountries = countryNames.filter(country => country.toLowerCase().includes(value.toLowerCase()));
+    setCountries(filteredCountries);
+  }
 
   return (
-    <>
-    </>
+      <div>
+        filter shown with: <input value={filter} onChange={handleFilterChange}/>
+        {countries.length < 10 ?
+          <ul>
+            {countries.map(country => (
+              <li key={country}>{country}</li>
+            ))}
+          </ul>
+          : <p>Too many matches, specify another filter</p>
+        }
+      </div>
   )
 }
 
