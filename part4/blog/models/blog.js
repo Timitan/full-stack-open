09e5481155020ac789d4295d74c9c074b01bb146
Timothy Tan <60724344+Timitan@users.noreply.github.com>
2026-01-11 12:51:07 -0800

    add exercise 4.2

---
+++ part4/blog/models/blog.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl, { family: 4 })
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
